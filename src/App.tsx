34d7d2c6de9c49981f0c975b252ba181309366be
anthony sanchez <anthonysa0813@gmail.com>
2022-06-28 00:50:30 -0500

    initilized proyect with reduxt boiled

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./reducers/tiendaReducer";
 
-function App() {
+const App = () => {
+  const store = createStore(reducer);
+  console.log(store.getState());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <h1>jiji</h1>
+    </Provider>
   );
-}
+};
 
 export default App;
---
+++ src/reducers/tiendaReducer.ts
@@ -0,0 +1,13 @@
+const initialState = {
+  products: [
+    { name: "product1", price: 133 },
+    { name: "product2", price: 232 },
+  ],
+  carrito: [],
+};
+
+const reducer = (state: Object = initialState, action: Object) => {
+  return state;
+};
+
+export default reducer;
